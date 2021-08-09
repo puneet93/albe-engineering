ae534ab04595226828e25b3319f54963e7ce72c7
puneet93 <puneet.softweb@gmail.com>
2021-08-09 19:39:55 +0530

    upload

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function TeamMember() {
     return (
       <div className="team-member">
@@ -8,17 +7,5 @@ function TeamMember() {
       </div>
     );
   }
-  
-=======
-function TeamMember() {
-    return (
-      <div className="team-member">
-        <img src="/team-image.png" className="img-fluid" alt="team" />
-        <h6 className="text-normal mb-0">John Doe</h6>
-        <p>Engineer</p>
-      </div>
-    );
-  }
-  
->>>>>>> 542e649c145194fc95be9192c9575c66ff9c7f04
+
   export default TeamMember;
--- components/services/ProjectManagement.js
+++ components/services/ProjectManagement.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from 'react';
 import { Row, Col, Modal } from 'react-bootstrap';
 
@@ -45,52 +44,4 @@ function ProjectManagement() {
     );
   }
   
-=======
-import React, {useState} from 'react';
-import { Row, Col, Modal } from 'react-bootstrap';
-
-function ProjectManagement() {
-    const [Show, setShow] = useState(false);
-  
-    return (
-      <>
-        <div className="service-card" onClick={() => setShow(true)}>
-            <img src="/project.png" srcSet="/project@2x.png" className="img-fluid" alt="project" />
-            <div className="service-card-overlay d-flex flex-column">
-                <h5>Project <br />Management</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                <span className="btn-link mt-auto"><img src="/arrow-right.png" alt="arrow" /> Read more</span>
-            </div>
-
-
-        </div>
-
-        <Modal
-          centered
-          size="lg"
-          show={Show}
-          className="service-modal"
-          onHide={() => setShow(false)}
-          aria-labelledby="example-modal-sizes-title-sm"
-        >
-          <Modal.Header closeButton></Modal.Header>
-          <Modal.Body className="p-0">
-              <Row className="m-0">
-                  <Col md="4" className="service-popup-image p-0" style={{backgroundImage: 'url(/project.png)'}}></Col>
-                  <Col md="8" className="p-0">
-                      <div className="service-popup-content">
-                          <h3 className="text-blue text-normal">Project Management</h3>
-                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it</p>
-                      </div>
-                  </Col>
-              </Row>
-          </Modal.Body>
-        </Modal>
-        
-      </>
-    );
-  }
-  
->>>>>>> 542e649c145194fc95be9192c9575c66ff9c7f04
   export default ProjectManagement;
