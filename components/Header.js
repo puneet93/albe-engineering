cfdfbbbc9ac37dd627d234c9d59c8dbd09ef75ea
puneet93 <puneet.softweb@gmail.com>
2021-08-10 19:16:03 +0530

    upload

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,22 @@
+import React, {useState} from 'react';
 import Image from 'next/image'
 import {Navbar, Container, Nav} from 'react-bootstrap'
 
 function Header() {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar className="header w-100" expand="md">
+        <Navbar className="header w-100" expanded={expanded} expand="md">
             <Container>
-                <Navbar.Brand href="/"><Image src="/logo-dark.svg" className="img-fluid" width="249" height="48" alt="logo" /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Brand href="#home"><Image src="/logo-dark.svg" className="img-fluid" width="249" height="48" alt="logo" /></Navbar.Brand>
+                <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className={expanded ? 'show' : ''} id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#about-us">About Us</Nav.Link>
-                        <Nav.Link href="#our-team">Team</Nav.Link>
-                        <Nav.Link href="#contact-us">Contact</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#home">Home</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#services">Services</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#about-us">About Us</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#our-team">Team</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#contact-us">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
